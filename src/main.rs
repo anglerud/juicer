4f732b278d51745eaf25df167fdf1dcf7c4b0f9e
Rikard Anglerud <rikard@anglerud.com>
2024-02-25 16:18:18 +0000

    Checkpoint

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use ratatui::{
+    layout::{Constraint, Direction, Layout},
     prelude::{CrosstermBackend, Frame, Terminal},
-    widgets::Paragraph,
+    style::{Color, Style},
+    widgets::{Block, Borders, Paragraph},
 };
+use tui_textarea::{Input, TextArea};
 
 struct App {
     counter: i64,
@@ -16,8 +19,7 @@ struct App {
 
 fn startup() -> Result<()> {
     enable_raw_mode()?;
-    execute!(std::io::stderr(), EnterAlternateScreen)?;
-    Ok(())
+    execute!(std::io::stderr(), EnterAlternateScreen)?; Ok(())
 }
 
 fn shutdown() -> Result<()> {
@@ -30,8 +32,49 @@ fn render_counter(app: &App) -> String {
     format!("Counter: {}", app.counter)
 }
 
+fn validate_input(textarea: &mut TextArea) -> bool {
+    if let Err(err) = textarea.lines()[0].parse::<f64>() {
+        textarea.set_style(Style::default().fg(Color::LightRed));
+        textarea.set_block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title(format!("ERROR: {}", err)),
+        );
+        false
+    } else {
+        textarea.set_style(Style::default().fg(Color::LightGreen));
+        textarea.set_block(Block::default().borders(Borders::ALL).title("OK"));
+        true
+    }
+}
+
+// XXX: This is very custom right now
 fn ui(app: &App, f: &mut Frame) {
-    f.render_widget(Paragraph::new(render_counter(&app)), f.size());
+    let layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![
+                Constraint::Percentage(50),
+                Constraint::Percentage(50),
+        ]);
+            // [Constraint::Length(3), Constraint::Min(1)].as_slice());
+    let layout_chunks = layout.split(f.size());
+
+    let input_layout = Layout::default().constraints([Constraint::Length(3),
+        Constraint::Min(1)].as_slice());
+    let input_layout_chunks = input_layout.split(layout_chunks[0]);
+
+    let mut textarea = TextArea::default();
+    textarea.set_cursor_line_style(Style::default());
+    textarea.set_placeholder_text("Enter a valid float (e.g. 1.56)");
+
+    // result (bool) not used
+    validate_input(&mut textarea);
+
+    let textarea_widget = textarea.widget();
+    f.render_widget(textarea_widget, input_layout_chunks[0]);
+    f.render_widget(
+        Paragraph::new(render_counter(&app)), layout_chunks[1]
+    );
 }
 
 fn update(app: &mut App) -> Result<()> {
